e4de7ffce86db02b13225508f46b1fb86dd2ccdd
Squeaky <squeaky_pl@gmx.com>
2016-09-18 20:59:51 -0300

    update package

--- runopt.sh
+++ runopt.sh
@@ -7,6 +7,7 @@ cat <<EOF
 -v `pwd`/src:/src
 -v `pwd`/build_deps:/src/build_deps
 -v `pwd`/build:/src/build
+-v `pwd`/package:/src/package
 -v `pwd`/devtools:/opt/devtools-6.2
 
 --env-file env.list
