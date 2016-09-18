942ba5f3f3858736d79dcac37b8c13c21a26e0ce
Squeaky <squeaky_pl@gmx.com>
2016-09-18 20:52:17 -0300

    update for GCC 6.2 and docker

---
+++ runopt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cat <<EOF
+
+-v `pwd`/src:/src
+-v `pwd`/build_deps:/src/build_deps
+-v `pwd`/build:/src/build
+-v `pwd`/devtools:/opt/devtools-6.2
+
+--env-file env.list
+
+-w /src
+
+EOF
