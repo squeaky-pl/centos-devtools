2bc484c8c5812cad285da6f1c4a05edf4dbb000d
Squeaky <squeaky_pl@gmx.com>
2017-06-17 10:25:11 +0200

    make deps for devtools-7.1

--- runopt.sh
+++ runopt.sh
@@ -8,8 +8,9 @@ cat <<EOF
 -v `pwd`/build_deps:/src/build_deps
 -v `pwd`/build:/src/build
 -v `pwd`/package:/src/package
--v `pwd`/devtools:/opt/devtools-6.3
+-v `pwd`/devtools:/opt/devtools-7.1
 
+--cpus=1
 --env-file env.list
 
 -w /src
